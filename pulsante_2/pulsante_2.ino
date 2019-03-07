9f90c94ceebffcfeea7796d9e8a0a13ee8704e77
leonardo <leonardo.basso@barsanti.edu.it>
2019-03-07 18:04:07 +0100

    07/03/2019 - modifiche

--- pulsante_2/pulsante_2.ino
+++ pulsante_2/pulsante_2.ino
@@ -1,9 +1,9 @@
 int led1 = 8; // si definisce il led e lo si assegna ad un determinato PIN
 int pushBottone = 9; // si definisce il bottone 
 int btn_status = 0; // si usa come variabile per indicare se il pulsante si premuto o meno e si utilizzerà nel corso del codice.
-long tempo_random;
-unsigned long tempo_bottone = 0;
-unsigned long tempo_riflesso = 0;
+int tempo_random;
+int tempo_bottone;
+int tempo_riflesso;
 void setup() {
   // put your setup code here, to run once:
 pinMode(led1, OUTPUT);
@@ -12,26 +12,26 @@ Serial.begin(9600);
 }
 
 void loop() {
+  tempo_bottone = 0;
+  tempo_riflesso = 0;
   tempo_random = random(2000, 10000);
-  delay(tempo_random);
-  tempo_random = millis();// parte a contare i millisecondi necessari ad accendere il led econtinua fino al push del bottone.
+  delay(tempo_random);// parte a contare i millisecondi necessari ad accendere il led econtinua fino al push del bottone.
   digitalWrite(led1, HIGH);
   while(btn_status == LOW)
   btn_status = digitalRead(pushBottone);
+  tempo_bottone = millis(); // restituisce i millisecondi necessari al led per essere spento dal bottone 
   if(btn_status == HIGH)
   {
     digitalWrite(led1, LOW);
-    
   }
   btn_status = LOW;
-  tempo_bottone = millis(); // restituisce i millisecondi necessari al led per essere spento dal bottone 
+    
   tempo_riflesso = tempo_bottone - tempo_random;
   Serial.println("tempo random: ");
   Serial.println(tempo_random);
-  Serial.println("tempo del push del bottone:  ");
-  Serial.println(tempo_bottone);
-  Serial.println("tempo riflesso: ");
+  Serial.println("tempo riflessi: ");
   Serial.println(tempo_riflesso);
   while(Serial.available() == 0);
-  delay(1000);
+  delay(100);
+  tempo_random = 0;
 }
