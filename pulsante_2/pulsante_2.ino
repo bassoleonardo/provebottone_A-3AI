6ece62cd597ab1c80be99a91b4a548abad2bd68a
leonardo <leonardo.basso@barsanti.edu.it>
2019-03-08 16:46:03 +0100

    08/03/2019 - prove con il buzzer

--- pulsante_2/pulsante_2.ino
+++ pulsante_2/pulsante_2.ino
@@ -2,10 +2,9 @@ int beep = 10;
 int led1 = 9; // si definisce il led e lo si assegna ad un determinato PIN
 int pushBottone = 8; // si definisce il bottone 
 int btn_status = 0; // si usa come variabile per indicare se il pulsante si premuto o meno e si utilizzerà nel corso del codice.
-long tempo_random;
-long tempo_random2;
-long tempo_bottone;
+int tempo_random;
 int tempo_riflesso;
+int tempo_riflesso_buzzer;
 void setup() {
   // put your setup code here, to run once:
 pinMode(led1, OUTPUT);
@@ -15,40 +14,43 @@ Serial.begin(9600);
 }
 
 void loop() { 
-  tempo_bottone = 0; 
-  digitalWrite(beep, LOW); // setta inizialmente il buzzer senza alcun tipo di suono
+  digitalWrite(beep, LOW);
+  Serial.println("INIZIO");
   tempo_random = random(2000, 10000); // genera un tempo random
-  delay(tempo_random);// parte a contare i millisecondi necessari ad accendere il led econtinua fino al push del bottone.
-  // con la funzione millis() riesce a prendere i millisecondi durante i quali viene acceso il led 
-  digitalWrite(led1, HIGH);
-  tempo_random = millis();
-  // questo while permette di capire se il bottone è stato premuto oppure no.
+  delay(tempo_random);// parte a contare i millisecondi necessari ad accendere il led econtinua fino al push del bottone.                      
+  digitalWrite(led1, HIGH); 
+  long tempo_led = millis();
+  
   while(btn_status == LOW)
+  
   btn_status = digitalRead(pushBottone);
-   
+  long tempo_bottone = millis(); // restituisce i millisecondi necessari al led per essere spento dal bottone 
   if(btn_status == HIGH)
   {
-    tempo_bottone = millis(); // restituisce i millisecondi necessari al led per essere spento dal bottone 
     digitalWrite(led1, LOW);
     btn_status = LOW;
   }
-  
-  // parte correlata alla seriale che permette di restituire in out il valore dei millisecondi i riflesso.
-  tempo_riflesso = tempo_bottone - tempo_random;
+  tempo_riflesso = tempo_bottone - tempo_led;
   Serial.println("Hai premuto il bottone dopo: ");
   Serial.println(tempo_riflesso);
-  
-  tempo_random2 = random(2000, 10000);
-  delay(tempo_random2);
+  tempo_random = random(2000, 10000);
+  delay(tempo_random);
   digitalWrite(beep, HIGH);
+  long tempo_buzzer = millis();
+
   while(btn_status == LOW)
-  
+  btn_status = digitalRead(pushBottone);
+  long tempo_bottone2 = millis();
   if(btn_status == HIGH)
   {
-    tempo_bottone = millis(); // restituisce i millisecondi necessari al led per essere spento dal bottone 
     digitalWrite(beep, LOW);
-    btn_status = LOW;
+    btn_status = LOW;  
   }
-  
+  tempo_riflesso_buzzer = tempo_bottone2 - (tempo_led + tempo_buzzer);
+  Serial.println("Hai premuto il bottone (con il buzzer) dopo: ");
+  Serial.println(tempo_riflesso_buzzer);
+  Serial.println("per restartare premere invio...");
   while(Serial.available() == 0);
+  Serial.println("----------------------------------------");
+  tempo_bottone = 0;
 }
