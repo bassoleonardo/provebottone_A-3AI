229f7822115a42dc62acddd460ee50459f0c189e
leonardo <leonardo.basso@barsanti.edu.it>
2019-03-08 15:05:16 +0100

    08/03/2019 - prove

--- pulsante_2/pulsante_2.ino
+++ pulsante_2/pulsante_2.ino
@@ -1,19 +1,21 @@
+int beep = 10;
 int led1 = 9; // si definisce il led e lo si assegna ad un determinato PIN
 int pushBottone = 8; // si definisce il bottone 
 int btn_status = 0; // si usa come variabile per indicare se il pulsante si premuto o meno e si utilizzerà nel corso del codice.
 long tempo_random;
+long tempo_random2;
 long tempo_bottone;
 int tempo_riflesso;
 void setup() {
   // put your setup code here, to run once:
 pinMode(led1, OUTPUT);
 pinMode(pushBottone, INPUT);
+pinMode(beep, OUTPUT);
 Serial.begin(9600);
 }
 
-void loop() {
-  tempo_random = 0; // RESET DELLE VARIABILI
-  tempo_bottone = 0; // RESET DELLE VARIABILI
+void loop() { 
+  tempo_bottone = 0; 
   digitalWrite(beep, LOW); // setta inizialmente il buzzer senza alcun tipo di suono
   tempo_random = random(2000, 10000); // genera un tempo random
   delay(tempo_random);// parte a contare i millisecondi necessari ad accendere il led econtinua fino al push del bottone.
@@ -35,9 +37,18 @@ void loop() {
   tempo_riflesso = tempo_bottone - tempo_random;
   Serial.println("Hai premuto il bottone dopo: ");
   Serial.println(tempo_riflesso);
-  while(Serial.available() == 0);
-  tempo_random = random(2000, 10000);
-  digitalWrite(
-  delay(tempo_random);
   
+  tempo_random2 = random(2000, 10000);
+  delay(tempo_random2);
+  digitalWrite(beep, HIGH);
+  while(btn_status == LOW)
+  
+  if(btn_status == HIGH)
+  {
+    tempo_bottone = millis(); // restituisce i millisecondi necessari al led per essere spento dal bottone 
+    digitalWrite(beep, LOW);
+    btn_status = LOW;
+  }
+  
+  while(Serial.available() == 0);
 }
