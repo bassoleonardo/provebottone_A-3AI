be05c90e714fa81423b020d50a1ba7cb5bc64036
leonardo <leonardo.basso@barsanti.edu.it>
2019-03-08 17:07:47 +0100

    08/03/2019 - completamento led e buzzer

--- pulsante_2/pulsante_2.ino
+++ pulsante_2/pulsante_2.ino
@@ -1,10 +1,11 @@
-int beep = 10;
-int led1 = 9; // si definisce il led e lo si assegna ad un determinato PIN
-int pushBottone = 8; // si definisce il bottone 
-int btn_status = 0; // si usa come variabile per indicare se il pulsante si premuto o meno e si utilizzerà nel corso del codice.
-int tempo_random;
-int tempo_riflesso;
-int tempo_riflesso_buzzer;
+int pushBottone = 13; // input del BOTTONE
+int led1 = 12; // LED
+int beep = 11; 
+int btn_status = 0; // si usa come variabile per indicare se il pulsante si premuto o meno e si utilizzerà nel corso del codice. (status del BOTTONE)
+int tempo_random; // tempo RANDOM
+int tempo_riflesso; // tempo del riflesso del LED
+int tempo_riflesso_buzzer; // tempo del riflesso del BUZZER
+
 void setup() {
   // put your setup code here, to run once:
 pinMode(led1, OUTPUT);
@@ -15,29 +16,28 @@ Serial.begin(9600);
 
 void loop() { 
   
-  Serial.println("INIZIO");
-  tempo_random = random(2000, 10000); // genera un tempo random
-  delay(tempo_random);// parte a contare i millisecondi necessari ad accendere il led econtinua fino al push del bottone.                      
+  Serial.println("INIZIO (i tempi sono misurati in millisecondi...");
+  tempo_random = random(2000, 10000); 
+  delay(tempo_random);                  
   digitalWrite(led1, HIGH); 
-  long tempo_led = millis();
-  
+  long tempo_led = millis(); // restituisce quando si accende il led1 (in millisecondi)
   while(btn_status == LOW)
   
   btn_status = digitalRead(pushBottone);
-  long tempo_bottone = millis(); // restituisce i millisecondi necessari al led per essere spento dal bottone 
+  long tempo_bottone = millis(); // restituisce il push del bottone
   check(led1);
   tempo_riflesso = tempo_bottone - tempo_led;
   Serial.println("Hai premuto il bottone dopo: ");
   Serial.println(tempo_riflesso);
+  
   tempo_random = random(2000, 10000);
   delay(tempo_random);
   digitalWrite(beep, HIGH);
-  long tempo_buzzer = millis();
-
+  long tempo_buzzer = millis(); // restituisce quando si accende il BUZZER (in millisecondi)
   while(btn_status == LOW)
   
   btn_status = digitalRead(pushBottone);
-  long tempo_bottone2 = millis();
+  long tempo_bottone2 = millis(); // restituisce il push del bottone
   check(beep);
   tempo_riflesso_buzzer = tempo_bottone2 - tempo_buzzer;
   Serial.println("Hai premuto il bottone (con il buzzer) dopo: ");
