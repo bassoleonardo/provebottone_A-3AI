d52ecd8bf1f51c01155c650f50407da686e6261f
leonardo <leonardo.basso@barsanti.edu.it>
2019-03-16 10:59:32 +0100

    16/03/2019 - programma QUASI finito

--- pulsante_2/pulsante_2.ino
+++ pulsante_2/pulsante_2.ino
@@ -4,8 +4,8 @@ LiquidCrystal lcd(8, 9, 10, 11, 12, 13);
 int pushBottone_restart = 2;
 int pushBottone = 3; // input del BOTTONE
 int led1 = 4; // LED
-int beep = 5; 
-int btn_status = 0; // si usa come variabile per indicare se il pulsante si premuto o meno e si utilizzerà nel corso del codice. (status del BOTTONE)
+int beep = 6; 
+int btn_status = 1; // si usa come variabile per indicare se il pulsante si premuto o meno e si utilizzerà nel corso del codice. (status del BOTTONE)
 int tempo_random; // tempo RANDOM
 int tempo_riflesso; // tempo del riflesso del LED
 int tempo_riflesso_buzzer; // tempo del riflesso del BUZZER
@@ -22,18 +22,21 @@ lcd.begin(16, 2);
 
 void loop() { 
   // CONDIZIONI INIZIALI PER LO START CORRETTO DEL PROGRAMMA
-  btn_status = LOW;
+  btn_status = HIGH;
   lcd.clear();
+  digitalWrite(beep, LOW);
   
   // --------- SEZIONE DEDICATA AL LED --------- //
   Serial.println("INIZIO (i tempi sono misurati in millisecondi)...");
+  lcd.print("INIZIO");
   tempo_random = random(2000, 10000); 
   delay(tempo_random);                  
   digitalWrite(led1, HIGH); 
   long tempo_led = millis(); // restituisce quando si accende il led1 (in millisecondi)
   
-  while(btn_status == LOW)
+  while(btn_status == HIGH)
   btn_status = digitalRead(pushBottone);
+  lcd.clear();
   long tempo_bottone = millis(); // restituisce il push del bottone
   check(led1);
   tempo_riflesso = tempo_bottone - tempo_led;
@@ -48,7 +51,7 @@ void loop() {
   digitalWrite(beep, HIGH);
   long tempo_buzzer = millis(); // restituisce quando si accende il BUZZER (in millisecondi)
   
-  while(btn_status == LOW)
+  while(btn_status == HIGH) // DA CAMBIARE IN CASO NON FUNZIONI
   btn_status = digitalRead(pushBottone);
   long tempo_bottone2 = millis(); // restituisce il push del bottone
   check(beep);
@@ -60,16 +63,16 @@ void loop() {
 
   // --------- SEZIONE DEDICATA AL PULSANTE DI RESTART --------- //
   Serial.println("premere il bottone di restart per iniziare: ");
-  while(btn_status == LOW)
+  while(btn_status == HIGH)
   btn_status = digitalRead(pushBottone_restart);
   Serial.println();
   Serial.println("-----------------------------------");
 }
 
 void check(int a){
-    if(btn_status == HIGH)
+    if(btn_status == LOW)
     {
       digitalWrite(a, LOW);
-      btn_status = LOW;  
+      btn_status = HIGH;  
     }
   }
